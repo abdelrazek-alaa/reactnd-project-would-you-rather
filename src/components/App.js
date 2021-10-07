a9d03397c29377a24ffec35cbf266870031d422e
Abdelrazek <abdelrazekhdd@gmail.com>
2021-10-07 22:18:32 +0200

    Get Initial Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import React, { Component } from "react";
 import "../App.css";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
-function App() {
-  return <div>hello world</div>;
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return <div>hello world</div>;
+  }
 }
 
-export default App;
+export default connect()(App);
