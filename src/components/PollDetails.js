c3e1ce7f53133ee580d5594dd7eed9883dfdd09f
Abdelrazek <abdelrazekhdd@gmail.com>
2021-10-12 20:54:25 +0200

    PollDetails Component 404 Page

--- src/components/PollDetails.js
+++ src/components/PollDetails.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { Component } from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
@@ -7,36 +7,44 @@ import Typography from "@mui/material/Typography";
 import { Grid } from "@mui/material";
 import QuestionOption from "./QuestionOption";
 import { connect } from "react-redux";
+import ErrorPage from "./ErrorPage";
 
-function PollDetails(props) {
-  const { authorName, authorAvatarURL, optionOne, optionTwo, question } = props;
+class PollDetails extends Component {
+  render() {
+    const { authorName, authorAvatarURL, optionOne, optionTwo, question } =
+      this.props;
 
-  return (
-    <Grid mt={2} container item direction="row" justifyContent="center">
-      <Card sx={{ border: 1, borderColor: "divider" }}>
-        <CardHeader
-          avatar={<Avatar src={authorAvatarURL} aria-label="avatar"></Avatar>}
-          title={`Asked by ${authorName}`}
-        />
-        <CardContent>
-          <Typography variant="h6">Results:</Typography>
-          <QuestionOption question={question} optionOne={optionOne} />
-          <QuestionOption question={question} optionTwo={optionTwo} />
-        </CardContent>
-      </Card>
-    </Grid>
-  );
+    if (question === null) {
+      return <ErrorPage />;
+    }
+
+    return (
+      <Grid mt={2} container item direction="row" justifyContent="center">
+        <Card sx={{ border: 1, borderColor: "divider" }}>
+          <CardHeader
+            avatar={<Avatar src={authorAvatarURL} aria-label="avatar"></Avatar>}
+            title={`Asked by ${authorName}`}
+          />
+          <CardContent>
+            <Typography variant="h6">Results:</Typography>
+            <QuestionOption question={question} optionOne={optionOne} />
+            <QuestionOption question={question} optionTwo={optionTwo} />
+          </CardContent>
+        </Card>
+      </Grid>
+    );
+  }
 }
 
 function mapStateToProps({ questions, users }, { questionId }) {
   const question = questions[questionId];
 
   return {
-    question,
-    authorName: users[question.author].name,
-    authorAvatarURL: users[question.author].avatarURL,
-    optionOne: question.optionOne.text,
-    optionTwo: question.optionTwo.text,
+    question: question ? question : null,
+    authorName: question ? users[question.author].name : null,
+    authorAvatarURL: question ? users[question.author].avatarURL : null,
+    optionOne: question ? question.optionOne.text : null,
+    optionTwo: question ? question.optionTwo.text : null,
   };
 }
 
